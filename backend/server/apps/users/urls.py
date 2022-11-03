360edd2b95e6d9cd9ec3af5fd4981f88100ec1aa
Kamwaro-001 <jobkamwaro9@gmail.com>
2022-11-03 15:22:55 +0300

    backend - adding user profiles

--- backend/server/apps/users/urls.py
+++ backend/server/apps/users/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path, include
-# from .views import UserDetail, UserList
 from apps.users import views
-
-# urlpatterns = [
-#     path('', UserList.as_view()),
-#     path('<int:pk>', UserDetail.as_view()),
-# ]
 
 urlpatterns = [
     path('', views.user_list),
