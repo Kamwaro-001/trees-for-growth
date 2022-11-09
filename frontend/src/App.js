5fd4b82c30b14bf1ff71663395ca4571c2a931d9
Kamwaro-001 <jobkamwaro9@gmail.com>
2022-11-09 23:19:16 +0300

    adding some routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
 // the routes
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/authentication/register/Register';
+import Login from './components/authentication/login/Login';
+import Home from './components/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+   return (
+      <div className="App">
+         <Router>
+            <Navbar />
+            <Routes>
+               <Route exact path='/' element={<Home />} />
+               <Route path='/login' element={<Login />} />
+               <Route path='/register' element={<Register />} />
+            </Routes>
+            <Footer />
+         </Router>
+      </div>
+   );
 }
 
 export default App;
