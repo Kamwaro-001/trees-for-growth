701ceb4d93ca8008150175b83965d81323e78fa0
Kamwaro-001 <jobkamwaro9@gmail.com>
2022-11-09 23:05:00 +0300

    initializing frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+// the routes
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Navbar.css"
 
-function Navbar() {
+const Navbar = () => {
   return (
     <div>Navbar</div>
   )
