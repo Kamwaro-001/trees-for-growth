c1d60cf3941312d832d72a71e42a43af88a079d6
Kamwaro-001 <jobkamwaro9@gmail.com>
2022-12-23 18:40:24 +0300

    updat[ing dashboard

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -23,60 +23,56 @@ const Dashboard = () => {
       lists.push(e)
     })
   });
-  // console.log(persona);
+
   const goals = [5, 10, 15, 20, 30, 50, 100, 200];
-  l = lists.length + 11;
-  // console.log(lists);
+
+  l = lists.length;
+
   let passed = goals.filter(g => g <= l)
   let goal = (goals[passed.length]);
   let doneGoals = passed.length
 
   return (
-    <>
-      <div id="dashboard">
-        
-        <section id="what-we-do" className="what-we-do">
-          <div className="container">
+    <section id="dashboard" className="dashboard">
+      <div className="container">
 
-            <div className="section-title">
-              <h2>Dashboard</h2>
-              <p>Here is most of your progress</p>
+        <div className="section-title">
+          <h2>Dashboard</h2>
+          <p>Here is most of your progress</p>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div className="icon-box">
+              <div className="icon"><i className="bx bxl-dribbble"><Icons.Tree /></i></div>
+              <h4><Link to="/trees">{l}</Link></h4>
+              <p className="dash-p">Number of Trees Planted</p>
+              <p>You can also join a community for motivation!</p>
             </div>
-
-            <div className="row">
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-                <div className="icon-box">
-                  <div className="icon"><i className="bx bxl-dribbble"><Icons.Tree /></i></div>
-                  <h4><Link to="/trees">{l}</Link></h4>
-                  <p className="dash-p">Number of Trees Planted</p>
-                  <p>You can also join a community for motivation!</p>
-                </div>
-              </div>
-
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-                <div className="icon-box">
-                  <div className="icon"><i className="bx bx-file"><Icons.Bullseye /></i></div>
-                  <h4><Link to="/trees">{goal}</Link></h4>
-                  <p className="dash-p">My Next Goal</p>
-                  <p>Plant {goal - l} more trees to reach your next goal!</p>
-                </div>
-              </div>
-
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-                <div className="icon-box">
-                  <div className="icon"><i className="bx bx-tachometer"><Icons.Box /></i></div>
-                  <h4><Link to="/trees">{doneGoals}</Link></h4>
-                  <p className="dash-p">Passed Goals</p>
-                  <p>Not Satisfied? You know what to do!</p>
-                </div>
-              </div>
-
-            </div>
-
           </div>
-        </section>
+
+          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div className="icon-box">
+              <div className="icon"><i className="bx bx-file"><Icons.Bullseye /></i></div>
+              <h4><Link to="/trees">{goal}</Link></h4>
+              <p className="dash-p">My Next Goal</p>
+              <p>Plant {goal - l} more trees to reach your next goal!</p>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div className="icon-box">
+              <div className="icon"><i className="bx bx-tachometer"><Icons.Box /></i></div>
+              <h4><Link to="/trees">{doneGoals}</Link></h4>
+              <p className="dash-p">Passed Goals</p>
+              <p>Not Satisfied? You know what to do!</p>
+            </div>
+          </div>
+
+        </div>
+
       </div>
-    </>
+    </section>
   )
 }
 
